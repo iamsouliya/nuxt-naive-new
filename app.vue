51cc3a697456362db14cff250e20deece0eec907
iamsouliya <souliyatmv24@gmail.com>
2024-09-19 11:10:03 +0700

    init

---
+++ app.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+  import { NMessageProvider } from 'naive-ui'
+</script>
+
+<template>
+  <div>
+    <n-message-provider>
+      <NuxtPage />
+    </n-message-provider>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  import { useMessage } from 'naive-ui'
+  const message = useMessage()
+</script>
+
+<template>
+  <div>
+    <n-button @click="message.success('click')"> click </n-button>
+  </div>
+</template>
